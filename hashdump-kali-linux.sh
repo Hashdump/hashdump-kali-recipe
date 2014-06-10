13683de6d7b397b5ffa10da2b4e61c655a48d485
Cale Black <cablack@rams.colostate.edu>
2014-06-10 16:37:38 -0600

    Added license, fixed the lack of web browser.

--- hashdump-kali-linux.sh
+++ hashdump-kali-linux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Kali Linux ISO recipe for : Hashdump 
+# Kali Linux ISO recipe for : Hashdump
 #########################################################################################
 # Desktop 	: XFCE4
 # Metapackages	: kali-linux-full
-# ISO size 	: 2.7 GB 
-# Special notes	: A custom kali for hashdump without an autologin to prevent misuse of the lab 
+# ISO size 	: 2.7 GB
+# Special notes	: A custom kali for hashdump without an autologin to prevent misuse of the lab
 #		: This script is not meant to run unattended.
 # Look and Feel	: Custom wallpaper, configs, bashrc (terrorshell), and more
 # Background	: http://www.offensive-security.com/kali-linux/kali-linux-recipes/
@@ -31,6 +31,7 @@ xfce4
 xfce4-goodies
 vim
 vim-gtk
+iceweasel
 EOF
 
 lb config --bootappend-live "boot=live config noautologin hostname=hashdump"
